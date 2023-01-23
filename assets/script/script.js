cfc03ba8a593a8b317608c4e9ae1cfc2bd9571af
João Gabriel Vieira <joaogvieira21@gmail.com>
2023-01-23 14:45:26 -0300

    version 1

---
+++ assets/script/script.js
@@ -0,0 +1,86 @@
+
+
+
+
+let bt1 = document.getElementById("slide-bar1");
+bt1.style.backgroundColor = "rgb(255, 136, 0)"
+
+
+function fixedColor1() {
+    let bt1 = document.getElementById("slide-bar1");
+    let bt2 = document.getElementById("slide-bar2");
+    let bt3 = document.getElementById("slide-bar3");
+    let bt4 = document.getElementById("slide-bar4");
+    let bt5 = document.getElementById("slide-bar5");
+
+    bt1.style.backgroundColor = "rgb(255, 136, 0)";
+    bt2.style.backgroundColor = "transparent";
+    bt3.style.backgroundColor = "transparent";
+    bt4.style.backgroundColor = "transparent";
+    bt5.style.backgroundColor = "transparent";
+
+}
+
+function fixedColor2() {
+    let bt1 = document.getElementById("slide-bar1");
+    let bt2 = document.getElementById("slide-bar2");
+    let bt3 = document.getElementById("slide-bar3");
+    let bt4 = document.getElementById("slide-bar4");
+    let bt5 = document.getElementById("slide-bar5");
+   
+
+
+    bt1.style.backgroundColor = "transparent";
+    bt2.style.backgroundColor = "rgb(255, 136, 0)";
+    bt3.style.backgroundColor = "transparent";
+    bt4.style.backgroundColor = "transparent";
+    bt5.style.backgroundColor = "transparent";
+
+}
+
+function fixedColor3() {
+    let bt1 = document.getElementById("slide-bar1");
+    let bt2 = document.getElementById("slide-bar2");
+    let bt3 = document.getElementById("slide-bar3");
+    let bt4 = document.getElementById("slide-bar4");
+    let bt5 = document.getElementById("slide-bar5");
+
+    bt1.style.backgroundColor = "transparent";
+    bt2.style.backgroundColor = "transparent";
+    bt3.style.backgroundColor = "rgb(255, 136, 0)";
+    bt4.style.backgroundColor = "transparent";
+    bt5.style.backgroundColor = "transparent";
+
+}
+
+function fixedColor4() {
+    let bt1 = document.getElementById("slide-bar1");
+    let bt2 = document.getElementById("slide-bar2");
+    let bt3 = document.getElementById("slide-bar3");
+    let bt4 = document.getElementById("slide-bar4");
+    let bt5 = document.getElementById("slide-bar5");
+
+    bt1.style.backgroundColor = "transparent";
+    bt2.style.backgroundColor = "transparent";
+    bt3.style.backgroundColor = "transparent";
+    bt4.style.backgroundColor = "rgb(255, 136, 0)";
+    bt5.style.backgroundColor = "transparent";
+
+}
+
+function fixedColor5() {
+    let bt1 = document.getElementById("slide-bar1");
+    let bt2 = document.getElementById("slide-bar2");
+    let bt3 = document.getElementById("slide-bar3");
+    let bt4 = document.getElementById("slide-bar4");
+    let bt5 = document.getElementById("slide-bar5");
+
+    bt1.style.backgroundColor = "transparent";
+    bt2.style.backgroundColor = "transparent";
+    bt3.style.backgroundColor = "transparent";
+    bt4.style.backgroundColor = "transparent";
+    bt5.style.backgroundColor = "rgb(255, 136, 0)";
+
+}
+
+
